2298068eb7e1d4f16803893537666fb1eefd3f99
Mauricio <slashera4@gmail.com>
2020-07-22 20:41:21 +0200

    Running 2020

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import Application from './components/App'
-import React, { Component } from 'react';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import Application from './components/App';
 
-export default class App extends Component {
-  render(){
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
   return (
-     <Application/>
-    
+    <Application/>
   );
-}
-}
+};
 
+
+export default App;
